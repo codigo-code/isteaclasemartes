bccf21d52be2b551a636ea610ecce3b85751f7f0
dantewirall <dante@wirall.com.ar>
2021-05-18 21:35:39 -0300

    update

--- Clase10/app/src/main/java/com/istea/MainActivity.kt
+++ Clase10/app/src/main/java/com/istea/MainActivity.kt
@@ -3,10 +3,10 @@ package com.istea
 import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
 import com.istea.dao.DBHelper
 import com.istea.dto.Persona
 
@@ -15,20 +15,24 @@ class MainActivity : AppCompatActivity() {
     lateinit var nombre: EditText
     lateinit var dni: EditText
     lateinit var guardar: Button
+    lateinit var ver:Button
+    lateinit var persona_layout:LinearLayout
 
-    init {
-        nombre= findViewById(R.id.nombre)
-        dni = findViewById(R.id.dni)
-        guardar = findViewById(R.id.guardar)
-    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        nombre= findViewById(R.id.nombre)
+        dni = findViewById(R.id.dni)
+        guardar = findViewById(R.id.guardar)
+        val db = DBHelper(this,null)
+        ver = findViewById(R.id.verPersonas)
+        persona_layout= findViewById(R.id.personalaout)
         guardar.setOnClickListener(View.OnClickListener {
 
-            val db = DBHelper(this,null)
+
 
             // antes de guardar llamos al AlertDialog
 
@@ -51,6 +55,24 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        })
+
+
+
+        ver.setOnClickListener(View.OnClickListener {
+
+            var view = LayoutInflater.from(this).inflate(R.layout.persona_layout,null)
+
+            val nombre:TextView = view.findViewById(R.id.tx_nombre)
+            val dni :TextView = view.findViewById(R.id.tx_dni)
+
+            db.obtenerPersonas().forEach{item ->
+                nombre.text=item.nombre
+                dni.text=item.dni
+            }
+
+            persona_layout.removeAllViews()
+            persona_layout.addView(view)
         })
     }
 }
